4fb90d802548a6157aeef20829b5bd64be0d7b7f
bypassv5 <163147680+bypassv5@users.noreply.github.com>
2025-07-27 16:19:10 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local scriptURL = "https://raw.githubusercontent.com/bypassv5/SabChecker/refs/heads/main/script.lua"
+local scriptURL = "https://raw.githubusercontent.com/bypassv5/StealABrainrptFinder/refs/heads/main/main.lua"
 if queue_on_teleport then
     queue_on_teleport("loadstring(game:HttpGet('"..scriptURL.."'))()")
 end
